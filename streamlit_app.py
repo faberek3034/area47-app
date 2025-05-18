31b4a0198e1be7089d357697380e20045c4a6c66
faberek3034 <fabiberghold3@gmail.com>
2025-05-18 13:41:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+git add .
+git commit -m "Finale Chat-Verschlüsselungsversion von Lisa"
+git push
